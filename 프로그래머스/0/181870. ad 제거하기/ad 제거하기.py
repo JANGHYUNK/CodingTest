0989a6996720fb1fd6961648fc5d0d573dee7334
JANGHYUNK <137320432+JANGHYUNK@users.noreply.github.com>
2024-06-24 14:52:43 +0900

    [level 0] Title: ad 제거하기, Time: 0.22 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.py
@@ -0,0 +1,12 @@
+def solution(strArr):
+    
+    result = []
+    for str in strArr:
+        if 'ad' not in str:
+            result.append(str)
+        else:
+            continue
+
+
+    print(result)
+    return result
